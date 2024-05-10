20fb8f5c7abaf2a0de81850b248b32c3b43822af
YEONGBIN PAK <suplem07@naver.com>
2024-05-10 19:33:17 +0900

    changed google-services.json

--- app/src/main/java/com/gcu/anniversary/LoginActivity.java
+++ app/src/main/java/com/gcu/anniversary/LoginActivity.java
@@ -4,8 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 import android.widget.Toast;
 

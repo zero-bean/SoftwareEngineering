c2648ddb0b23fa016906986a4f81fce8cb5c4171
JoonKyo <joonkyolee@gmail.com>
2024-05-21 23:37:08 +0900

    프로필 수정 기능 추가

--- app/src/main/java/com/gcu/anniversary/HomeFragment.java
+++ app/src/main/java/com/gcu/anniversary/HomeFragment.java
@@ -12,9 +12,9 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.bumptech.glide.Glide;
-
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -25,19 +25,15 @@ import com.google.firebase.database.ValueEventListener;
 
 import DTO.UserData;
 
-
 public class HomeFragment extends Fragment {
-    // 디버깅을 위한 로그용 태그
-    private final String TAG ="HomeFragment";
-    // 파이어베이스 인증
+    private final String TAG = "HomeFragment";
     private FirebaseAuth mAuth;
-    // 현재 로그인한 구글 계정 사용자를 의미합니다
     private FirebaseUser currentUser;
-    // 파이어베이스의 특정 데이터를 수정하기 위해서 필요합니다
     private DatabaseReference userRef;
     private UserData myUserData;
     private ImageView profileImageView;
     private TextView profileNameTextView;
+    private Button changeProfileButton;
     private Button logoutBtn;
 
     public HomeFragment() {
@@ -47,11 +43,8 @@ public class HomeFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // 인증 서비스를 사용하기 위해서 인스턴스를 생성합니다.
         mAuth = FirebaseAuth.getInstance();
-        // 현재 로그인한 사용자의 계정 정보를 가져옵니다.
         currentUser = mAuth.getCurrentUser();
-        // 파이어베이스의 users 경로에 있는 본인의 데이터 경로에 접근합니다.
         userRef = FirebaseDatabase.getInstance().getReference()
                 .child("users").child(currentUser.getUid());
     }
@@ -59,12 +52,19 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_home, container, false);
 
         profileImageView = rootView.findViewById(R.id.myProfileImage);
         profileNameTextView = rootView.findViewById(R.id.profileNameTextView);
+        changeProfileButton = rootView.findViewById(R.id.changeProfileButton);
         logoutBtn = rootView.findViewById(R.id.logoutButton);
+
+        changeProfileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navigateToSetUserNameFragment();
+            }
+        });
 
         logoutBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -80,27 +80,23 @@ public class HomeFragment extends Fragment {
     public void onStart() {
         super.onStart();
         if (currentUser != null) {
-            // 현재 사용자의 정보를 일회성 리스너를 통해서 가져옵니다.
             userRef.addListenerForSingleValueEvent(new ValueEventListener() {
                 @Override
                 public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                    // 사용자의 정보가 존재한다면
                     if (dataSnapshot.exists()) {
                         Log.d(TAG, "FIREBASE 유저 데이터 갱신 시작");
-                        // 파이어베이스에 저장된 사용자의 정보를 가져옵니다.
                         UserData userData = dataSnapshot.getValue(UserData.class);
-                        // 사용자의 정보를 화면에 보여줍니다.
-                        updateUserProfile(userData);
-                    }
-                    // 사용자의 정보가 존재하지 않는다면
-                    else {
+                        if (userData != null && userData.getUserName() != null) {
+                            updateUserProfile(userData);
+                        } else {
+                            navigateToSetUserNameFragment();
+                        }
+                    } else {
                         Log.d(TAG, "FIREBASE 유저 데이터 초기화 시작");
-                        // 파이어베이스에 사용자 정보를 등록합니다.
                         initializeUserData();
                     }
                 }
 
-                // 특정 원인으로 인하여 데이터를 가져오는 것을 실패한다면, 실패했다는 로그를 출력합니다.
                 @Override
                 public void onCancelled(@NonNull DatabaseError databaseError) {
                     Log.e(TAG, "FIREBASE 유저 데이터 갱신 실패: ", databaseError.toException());
@@ -109,7 +105,6 @@ public class HomeFragment extends Fragment {
         }
     }
 
-    // 로그아웃 버튼을 누르면 로그아웃합니다.
     private void logout() {
         Intent intent = new Intent(requireContext(), LoginActivity.class);
         mAuth.signOut();
@@ -117,25 +112,31 @@ public class HomeFragment extends Fragment {
         requireActivity().finish();
     }
 
-    // 신규 사용자의 정보를 파이어베이스에 등록하고 프로필 화면에 정보를 전송합니다.
     private void initializeUserData() {
-        // 구글 계정에 저장된 유저의 정보를 가져와 초기화합니다.
         String userUID = currentUser.getUid();
         String displayName = currentUser.getDisplayName();
         String photoUrl = currentUser.getPhotoUrl() != null ? currentUser.getPhotoUrl().toString() : null;
 
-        UserData tmp = new UserData(userUID, displayName, photoUrl);
+        UserData tmp = new UserData(userUID, displayName, photoUrl, null);
         userRef.setValue(tmp);
-        updateUserProfile(tmp);
+        navigateToSetUserNameFragment();
     }
 
     private void updateUserProfile(UserData userData) {
         if (userData != null) {
-            Log.d(TAG, "유저 이름 갱신 성공, name:" + userData.getNickName());
-            profileNameTextView.setText(userData.getNickName());
+            Log.d(TAG, "유저 이름 갱신 성공, name:" + userData.getUserName());
+            profileNameTextView.setText(userData.getUserName());
 
             String photoUrl = userData.getImageURL();
             Glide.with(requireContext()).load(photoUrl).into(profileImageView);
         }
+    }
+
+    private void navigateToSetUserNameFragment() {
+        FragmentTransaction transaction = getParentFragmentManager().beginTransaction();
+        SetUserNameFragment setUserNameFragment = SetUserNameFragment.newInstance(true);
+        transaction.replace(R.id.fragment_container, setUserNameFragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
--- app/src/main/java/com/gcu/anniversary/MainActivity.java
+++ app/src/main/java/com/gcu/anniversary/MainActivity.java
@@ -3,66 +3,88 @@ package com.gcu.anniversary;
 import android.os.Bundle;
 import android.view.MenuItem;
 
-import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import DTO.UserData;
 
-// Single Activty Architecture 구조입니다
 public class MainActivity extends AppCompatActivity {
     FragmentManager fragmentManager;
     Fragment fragment[];
     BottomNavigationView bottomNavigationView;
+    FirebaseUser currentUser;
+    DatabaseReference userRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        fragmentManager = getSupportFragmentManager();
+        currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (currentUser != null) {
+            userRef = FirebaseDatabase.getInstance().getReference().child("users").child(currentUser.getUid());
+
+            userRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    UserData userData = dataSnapshot.getValue(UserData.class);
+                    if (userData != null && (userData.getUserName() == null || userData.getUserName().isEmpty())) {
+                        // If username is not set, navigate to SetUserNameFragment
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new SetUserNameFragment()).commit();
+                    } else {
+                        // If username is set, proceed with normal flow
+                        initializeFragments();
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+                    // Handle error
+                }
+            });
+        } else {
+            // Handle case where user is not authenticated
+        }
+
         bottomNavigationView = findViewById(R.id.bottom_navigation);
-
-        // Fragment를 초기화합니다
-        initializeFragments();
-
-
-        // 하단의 네비게이션 바를 통해서 프레그먼트 전환을 관리합니다
         bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
                 int id = menuItem.getItemId();
-                
                 if (id == R.id.navigation_bar_item_1) {
-                    getSupportFragmentManager().beginTransaction().replace(R.id.content_layout, new HomeFragment()).commit();
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
                 } else if (id == R.id.navigation_bar_item_2) {
-                    getSupportFragmentManager().beginTransaction().replace(R.id.content_layout, new FriendFragment()).commit();
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new FriendFragment()).commit();
                 } else if (id == R.id.navigation_bar_item_3) {
-                    getSupportFragmentManager().beginTransaction().replace(R.id.content_layout, new CalenderFragment()).commit();
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new CalendarFragment()).commit();
                 } else if (id == R.id.navigation_bar_item_4) {
-                    getSupportFragmentManager().beginTransaction().replace(R.id.content_layout, new AlertFragment()).commit();
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new AlertFragment()).commit();
                 }
-
                 return true;
             }
         });
-
     }
 
     private void initializeFragments() {
+        fragmentManager = getSupportFragmentManager();
         fragment = new Fragment[4];
         fragment[0] = new HomeFragment();
         fragment[1] = new FriendFragment();
-        fragment[2] = new CalenderFragment();
+        fragment[2] = new CalendarFragment();
         fragment[3] = new AlertFragment();
 
-        fragmentManager.beginTransaction().add(R.id.content_layout, fragment[0]).commit();
+        fragmentManager.beginTransaction().add(R.id.fragment_container, fragment[0]).commit();
     }
 }
